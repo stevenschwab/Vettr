90930b76729986e23b52437c38ea37d852ee3a6c
Steven Schwab <schwabs@umich.edu>
2022-12-13 13:35:41 -0600

    uncommitted files

---
+++ Vettr/Constants.swift
@@ -0,0 +1,20 @@
+//
+//  Constants.swift
+//  Vettr
+//
+//  Created by Steven Schwab on 10/30/22.
+//
+import Foundation
+import UIKit
+
+struct K {
+
+    struct BrandColors {
+        static let vettrBlue = hexStringToUIColor("#1A3E52")
+    }
+    
+    struct Supabase {
+        static let projectAPIkey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImF3bXZsdnFzbndtanBwdnVkbnRuIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY2NzkxNjM5OCwiZXhwIjoxOTgzNDkyMzk4fQ.ad14XdTKNYnOXqvq7eyv9-ogwMhpOSs8l9XkX4TKVM8"
+        static let projectURL = URL(string: "https://awmvlvqsnwmjppvudntn.supabase.co")!
+    }
+}
---
+++ Vettr/Model/EventSpeakers.swift
@@ -0,0 +1,12 @@
+//
+//  EventSpeakers.swift
+//  Vettr
+//
+//  Created by Steven Schwab on 12/4/22.
+//
+
+import Foundation
+
+struct EventSpeakers: Decodable {
+    var speakers: [Speaker]
+}
